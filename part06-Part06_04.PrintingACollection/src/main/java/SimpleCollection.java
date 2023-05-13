721547ea86fb221f75005f413ad98525893105c4
Christopher chandler <christopher.chandler@outlook.de>
2023-05-13 10:23:01 +0200

    6.4 complete

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,49 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    public String StringMessage (){
+
+        if (elements.size() == 0) {
+            return String.format("The collection %s is empty.",name);
+        }
+            if (elements.size() == 1) {
+                return String.format("The collection %s has %d element:\n",name,elements.size());
+            }
+        else {
+                return String.format("The collection %s has %d elements:\n",name,elements.size());
+            }
+
+
+    }
+
+    public String toString() {
+
+
+        if (elements.size() > 0) {
+
+        String newString ="";
+        int length = elements.size();
+        for (String ele: elements){
+            length--;
+            if (length != 0){
+
+
+            newString+=ele+"\n";
+             }
+
+            else{
+                newString+=ele;
+            }
+        }
+
+        return StringMessage()+newString;
+
+    }
+         else {
+
+            return StringMessage();
+    }
+    }
+
 }
