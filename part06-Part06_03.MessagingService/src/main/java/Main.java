9fa8635d6d92ba3fe33500f58e4a08da14b9c4d3
Christopher chandler <christopher.chandler@outlook.de>
2023-05-14 09:32:36 +0200

    Restructure
    Restructure

--- part06-Part06_03.MessagingService/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        MessagingService ms = new MessagingService();
-        System.out.println(ms.getMessages());
-    }
-}
