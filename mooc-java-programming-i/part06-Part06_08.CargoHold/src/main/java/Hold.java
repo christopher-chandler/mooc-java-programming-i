8a7b6bfc69944752ddcb829aa55d1f93956ed88e
Christopher chandler <christopher.chandler@outlook.de>
2023-06-09 07:25:48 +0200

    Finish 06_08

--- mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Hold.java
+++ mooc-java-programming-i/part06-Part06_08.CargoHold/src/main/java/Hold.java
@@ -1,12 +1,36 @@
+import java.util.ArrayList;
+
 public class Hold {
 
-    private int weight;
+    private int maxWeight;
+    private int suitcaseWeight = 0;
+    private ArrayList<Suitcase> suitcaseContainer = new ArrayList<>();
 
     public Hold(int weight){
-        this.weight = weight;
+        this.maxWeight = weight;
     }
 
 
+    public void addSuitcase (Suitcase suitcase) {
 
+        int intermediateWeight = suitcaseWeight+suitcase.totalWeight();
+        if ( intermediateWeight <= maxWeight){
+        suitcaseContainer.add(suitcase);
+        suitcaseWeight+=suitcase.totalWeight();
+
+        }
+
+    }
+
+    public String toString(){
+        return String.format(" %d suitcases in (%d kg)", suitcaseContainer.size(),suitcaseWeight);
+    }
+
+    public void printItems(){
+
+        for (Suitcase ele: suitcaseContainer) {
+             ele.printItems();
+        }
+    }
 
 }
