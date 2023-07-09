811116e09762b4d9de76d58f9f86afdbb85bc85c
Christopher chandler <christopher.chandler@outlook.de>
2023-07-09 18:57:32 +0200

    Continue 6.2

---
+++ mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/TodoList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+
+
+    private ArrayList<String> errands = new ArrayList<>();
+
+    public TodoList (){
+
+    }
+
+    public void add (String task){
+
+        errands.add(task);
+
+    }
+    public void print() {
+
+        int count = 1;
+        for (String task: errands) {
+            System.out.print(count);
+            System.out.println(": "+task);
+            count++;
+        }
+
+
+    }
+
+    public void remove (int number) {
+        errands.remove(number-1);
+
+    }
+
+
+}
